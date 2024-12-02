51a90106308453d23032d68f83b070c3a3a49331
osw00817 <42710245+osw00817@users.noreply.github.com>
2024-12-02 13:34:03 +0900

    [#2] typescript-express

---
+++ nodejs-typescript/src/index.ts
@@ -0,0 +1,26 @@
+import dotenv from "dotenv"
+import cors from "cors"
+import morgan from "morgan"
+import express, {Request,Response} from "express";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use(morgan("dev"));
+
+app.use(cors({
+    origin: "http://example.com",
+    methods: ["GET","POST"]
+}));
+
+app.get("/",(req:Request, res:Response) => {
+    res.send("Hello, Typescript with Express!");
+});
+
+app.listen(PORT,() => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
