5ef8431db51cfa690ebfcd9fe9936fdcdfb795c0
osw00817 <42710245+osw00817@users.noreply.github.com>
2024-12-30 02:46:44 +0900

    nestjs 1일차

--- Language/Nestjs/cat/src/app.controller.ts
+++ Language/Nestjs/cat/src/app.controller.ts
@@ -1,10 +1,11 @@
 import { Body, Controller, Get, Param, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Request } from 'express';
+import { CatsService } from './cats/cats.service';
 
 @Controller('cats')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly catsService: CatsService) {}
 
   // * localhost:3000/cats/hello
   @Get('hello/:id/:name')
